db9ee91c31571728cc674d116b249dbea0145b9a
x4nth055 <37851086+x4nth055@users.noreply.github.com>
2019-01-03 23:32:39 +0100

    fix login bug

--- daw_project/login.php
+++ daw_project/login.php
@@ -2,7 +2,7 @@
 
 include "includes/connection.php";
 
-if(isset($_POST['email']) && isset($_POST['pass']) && isset($_POST['radio'])) {
+if(isset($_POST['email']) && isset($_POST['pass'])) {
     $email = $_POST['email'];
     $pass  = $_POST['pass'];
 
