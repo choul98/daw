131b1d6c5931bbd473d91b7f67f67d83ae830fd8
x4nth055 <37851086+x4nth055@users.noreply.github.com>
2019-01-04 16:48:11 +0100

    added $data to scope

--- daw_project/includes/connection.php
+++ daw_project/includes/connection.php
@@ -7,4 +7,12 @@ catch(Exception $e)
 {
     die('Erreur : '.$e->getMessage());
 }
+
+session_start();
+if(isset($_SESSION['ID'])) {
+    $id = $_SESSION['ID'];
+    $result = $bdd->query("SELECT * FROM USER WHERE ID = '$id'");
+    $data = $result->fetch();
+}
+
 ?>
