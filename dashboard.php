2483ee5f1417349d80c34101f61f4d3c228e9038
x4nth055 <fullclip@protonmail.com>
2019-01-05 15:31:45 +0100

    Revert "finalize admin "
    This reverts commit 064e9685f2f71a9045a543b4c48b3384c3b012a2.

--- dashboard.php
+++
@@ -1,5 +0,0 @@
-<?php 
-    include "includes/header.php";
-    include "includes/sidebar.php";
-    include "includes/footer.php";
-?>
--- includes/logout.php
+++
@@ -1,5 +0,0 @@
-<?php
-    session_start();
-    $_SESSION['ID'] = null;
-    header("Location: ../index.php");
-?>
