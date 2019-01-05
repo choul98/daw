f5eb476427f1ed635dbaf95b7caf517419d8a903
Noanisss <46326660+Noanisss@users.noreply.github.com>
2019-01-05 19:38:32 +0100

    Add files via upload

--- daw_project/index.php
+++ daw_project/index.php
@@ -3,10 +3,41 @@
   include "includes/header.php";
 ?>
 
-<div class="container">
-  <div class='col-lg-4'>
+
+
+ <html lang="en">
+    <body>
+
+        <header>
+          <link rel="stylesheet" type="text/css" href="css/aa.css">
+          <!-- Navbar -->
+          
+          <!-- Navbar -->
+          <!-- Full Page Intro -->
+          <div class="view" style="background-image: url(''); background-repeat: no-repeat; background-size: cover; background-position: center center;">
+            <!-- Mask & flexbox options-->
+            <div class="mask rgba-gradient align-items-center">
+              <!-- Content -->
+              <div class="container">
+                <!--Grid row-->
+                <div class="row mt-5">
+                  <!--Grid column-->
+                  <div class="col-md-6 mb-5 mt-md-0 mt-5 white-text text-center text-md-left">
+                    <h1 class="h1-responsive font-weight-bold wow fadeInLeft" data-wow-delay="0.3s">Doctorat </h1>
+                    <hr class="hr-light wow fadeInLeft" data-wow-delay="0.3s">
+                    <h6 class="mb-3 wow fadeInLeft" data-wow-delay="0.3s">Site officiel pour Doctorants <br> Université Constantine 2 Abdelhamid Mahri
+                   </h6>
+                    <a class="btn btn-outline-white wow fadeInLeft" data-wow-delay="0.3s">Learn more</a>
+                  </div>
+                  <!--Grid column-->
+                  <!--Grid column-->
+                  
+
+  <div class='col-lg-3'>
+    <div class="container-fluid bg">
       <h3>Login</h3>
       <form method='post' action='login.php'>
+        <form class="form-container"</form>
         <div class="form-group">
           <label for="email">Email address</label>
           <input name='email' type="email" class="form-control" aria-describedby="emailHelp" placeholder="Enter Email">
