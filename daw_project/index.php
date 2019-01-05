ecfae1013898b606ec0ad09d40c3b9ada6d40f91
Noanisss <46326660+Noanisss@users.noreply.github.com>
2019-01-05 21:43:14 +0100

    Add files via upload

--- daw_project/index.php
+++ daw_project/index.php
@@ -14,7 +14,11 @@
           
           <!-- Navbar -->
           <!-- Full Page Intro -->
-          <div class="view" style="background-image: url(''); background-repeat: no-repeat; background-size: cover; background-position: center center;">
+          
+
+                  <!--Grid column-->
+                  <!--Grid column-->
+                 <div class="view" style="background-image: url(''); background-repeat: no-repeat; background-size: cover; background-position: center center;">
             <!-- Mask & flexbox options-->
             <div class="mask rgba-gradient align-items-center">
               <!-- Content -->
@@ -22,22 +26,21 @@
                 <!--Grid row-->
                 <div class="row mt-5">
                   <!--Grid column-->
-                  <div class="col-md-6 mb-5 mt-md-0 mt-5 white-text text-center text-md-left">
+                  <div class="col-md-8 mb-5 mt-md-0 mt-5 white-text text-center text-md-left">
                     <h1 class="h1-responsive font-weight-bold wow fadeInLeft" data-wow-delay="0.3s">Doctorat </h1>
                     <hr class="hr-light wow fadeInLeft" data-wow-delay="0.3s">
-                    <h6 class="mb-3 wow fadeInLeft" data-wow-delay="0.3s">Site officiel pour Doctorants <br> Université Constantine 2 Abdelhamid Mahri
+                    <h6 class="mb-3 wow fadeInLeft" data-wow-delay="0.3s">Site officiel pour Doctorants et Master <br> Université Constantine 2 Abdelhamid Mahri
                    </h6>
                     <a class="btn btn-outline-white wow fadeInLeft" data-wow-delay="0.3s">Learn more</a>
                   </div>
-                  <!--Grid column-->
-                  <!--Grid column-->
-                  
 
-  <div class='col-lg-3'>
-    <div class="container-fluid bg">
+
+  <div class='col-lg-3' style="border-color: Dark" >
+    
+      
       <h3>Login</h3>
       <form method='post' action='login.php'>
-        <form class="form-container"</form>
+        <form class="form-container" align="right">
         <div class="form-group">
           <label for="email">Email address</label>
           <input name='email' type="email" class="form-control" aria-describedby="emailHelp" placeholder="Enter Email">
@@ -47,11 +50,39 @@
           <label for="exampleInputPassword1">Password</label>
           <input name='pass' type="password" class="form-control" placeholder="Password">
         </div>
-
         <button type="submit" class="btn btn-success">Submit</button>
     </form>
   </div>
+  
 </div>
+<br>
+<br>
+
+
+
+
+
+
+<div class="row">
+  <div class="col-4">
+    <div class="list-group" id="list-tab" role="tablist">
+
+      <a class="list-group-item list-group-item-action active" id="list-home-list" data-toggle="list" href="#list-home" role="tab" aria-controls="home">SITW</a>
+      <a class="list-group-item list-group-item-action" id="list-profile-list" data-toggle="list" href="#list-profile" role="tab" aria-controls="profile">GL</a>
+      <a class="list-group-item list-group-item-action" id="list-messages-list" data-toggle="list" href="#list-messages" role="tab" aria-controls="messages">RSD</a>
+      <a class="list-group-item list-group-item-action" id="list-settings-list" data-toggle="list" href="#list-settings" role="tab" aria-controls="settings">STIC</a>
+    </div>
+  </div>
+  <div class="col-8">
+    <div class="tab-content" id="nav-tabContent">
+      <div class="tab-pane fade show active" id="list-home" role="tabpanel" aria-labelledby="list-home-list">Ce master a pour but, de former des étudiants ayant de bonnes connaissances en systèmes d’information, à assurer les fonctions d’ingénieur à forte compétence en conception et gestion de systèmes d’information et en conduite de projets dans les organisations. Il leur permet également d’intégrer une équipe de recherche à l’université.
+
+L’équipe pédagogique est attentive à délivrer un enseignement qui soit adapté aux besoins des entreprises, ouvert aux technologies novatrices et orienté vers la professionnalisation par de nombreux travaux dirigés/pratiques et un travail d’initiation à la recherche en relation avec les entreprises. Ce travail sera suivi par un enseignant de l’équipe pédagogique.</div>
+      <div class="tab-pane fade" id="list-profile" role="tabpanel" aria-labelledby="list-profile-list">La spécialité « Génie logiciel » a pour objectif d'apporter aux étudiants les compétences approfondies requises pour pouvoir intervenir dans toutes les phases de la production logiciel à la fois au niveau de la conception et la spécification (patrons de conception, UML ...), des architectures et composants logiciels (JEE ...), des IHM, de la qualité logicielle, de la validation logiciels, des ateliers de génie logiciels et des technologies du Web. La gestion de projet est aussi aborder pour la maîtrise des grands projets informatiques.</div>
+      <div class="tab-pane fade" id="list-messages" role="tabpanel" aria-labelledby="list-messages-list">De plus en plus des applications informatiques sont conçues pour fonctionner sur des réseaux d’ordinateurs coopérants. La consolidation de l’Internet mondial, l’émergence du Cloud Computing, les nouvelles technologies des réseaux de communications sans fils, et la disponibilité d’ordinateurs toujours moins chers et plus puissants permettent la construction d’une nouvelle génération d’applications distribuées. Ces applications envahissent l’industrie, le commerce, la santé, l’éducation et, en effet, la vie personnelle comme l’intégration de la technologie informatique et des communications numériques. Il y a quelques aspects de la vie qui ne sont pas améliorés par les réseaux et systèmes di stribués, mais qui doivent aussi être tempérés par les préoccupations de la sécurité et la durabilité de l’énergie. Ce Master « Réseaux et Systèmes distribués » offre une formation aussi bien théorique que pratique dans le domaine des réseaux et des systèmes distribués. De ce fait, les étudiants ayant validé la première année dans l’une des formations RSD, GL et STIC peuvent passer d’une spécialité à une autre. Ce passage nécessite l’acquisition de modules spécifiques à la formation choisie.</div>
+      <div class="tab-pane fade" id="list-settings" role="tabpanel" aria-labelledby="list-settings-list">Le Master des Sciences de l’informatique, option « Sciences et Technologie de l’Information et de la Communication » est un enseignement scientifique de deuxième cycle universitaire à finalité académique. Cette formation vise une insertion professionnelle des diplômés sous l’angle du développement et de la gestion de projets NTIC (sites Internet, Intranet, TV Web, CD Rom, Designer des projets multimédia, etc.). Ce domaine recouvre plusieurs spécialisations : concepteur/ développeur web, infographiste Internet, webmaster, graphiste multimédia, ingénieur des études web ou multimédia, infographiste, spécialiste de traitement d’images, etc. L’objectif global est de permettre aux candidats titulaires d’un master académique de pouvoir être apte à la résolution des problèmes des STIC d’une organisation. La première année (M1) est une année de tronc commun où seront fournis les concepts fondamentaux de base de l’informatique et des mathématiques. L’enseignement permet de donner aux étudiants une formation en spécifiant les aspects relatifs aux systèmes d’information (aussi bien au sens image, son et vidéo) et de la technologie de l’information et de la communication.</div>
+    </div>
+  </div>
 
     <?php
         include "includes/footer.php";
